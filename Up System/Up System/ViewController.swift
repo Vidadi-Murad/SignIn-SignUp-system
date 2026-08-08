66e494467ebafd204bacb0e8e16eec359f234efc
VidadiMurad <vidad0murad@gmail.com>
2026-08-08 13:51:48 +0400

    First Commit

---
+++ Up System/Up System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Up System
+//
+//  Created by Vidadi Muradzada on 08.08.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
